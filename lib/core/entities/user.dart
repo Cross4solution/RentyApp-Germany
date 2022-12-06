ad9da92704b3d1ffd5b53dce01dc06058c156686
yasinkrcn <23yasin1999@gmail.com>
2022-12-06 18:06:43 +0300

    user model created

--- lib/core/entities/user.dart
+++ lib/core/entities/user.dart
@@ -1,1 +1,39 @@
-class UserModel {}
+import '../_core_exports.dart';
+
+class UserModel {
+  final String name;
+  final String email;
+  final String phoneNumber;
+  final String username;
+  final String password;
+  final bool isSeller;
+
+  UserModel({
+    required this.name,
+    required this.email,
+    required this.phoneNumber,
+    required this.username,
+    required this.password,
+    required this.isSeller,
+  });
+
+  factory UserModel.fromMap(Map<String, dynamic> map) => UserModel(
+        name: map['name'],
+        email: map['email'],
+        phoneNumber: map['phoneNumber'],
+        username: map['username'],
+        password: map['password'],
+        isSeller: map['isSeller'],
+      );
+
+  Map<String, dynamic> toMap() => {
+        'name': name,
+        'email': email,
+        'phoneNumber': phoneNumber,
+        'username': username,
+        'password': password,
+        'isSeller': isSeller,
+      };
+
+  String toJson() => json.encode(toMap());
+}

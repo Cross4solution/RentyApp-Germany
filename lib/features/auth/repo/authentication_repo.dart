80512987626bb61dd5a22e842794a3e57d728b62
yasinkrcn <23yasin1999@gmail.com>
2022-12-06 18:06:15 +0300

    removed

--- lib/features/auth/repo/authentication_repo.dart
+++
@@ -1,8 +0,0 @@
-import 'package:dartz/dartz.dart';
-import '../../../../core/_core_exports.dart';
-
-abstract class AuthenticationRepo {
-  Future<Either<Failure, void>> register();
-
-  Future<Either<Failure, void>> signOut();
-}

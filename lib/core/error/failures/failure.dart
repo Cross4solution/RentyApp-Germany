e17b6668687dd7634cfff8baf6fedd27ec26bcec
yasinkrcn <23yasin1999@gmail.com>
2022-12-16 18:24:18 +0300

    fixed

--- lib/core/error/failures/failure.dart
+++ lib/core/error/failures/failure.dart
@@ -3,44 +3,46 @@ import 'package:equatable/equatable.dart';
 import '../../_core_exports.dart';
 
 abstract class Failure extends Equatable {
-  const Failure([List properties = const <dynamic>[]]);
+  String? errorMessage;
+  Failure([List properties = const <dynamic>[]]);
 }
 
 extension FailureExtension on Failure {
   void showErrorSnackBar() {
     switch (runtimeType) {
       case NullPointerFailure:
-        showCustomMessenger(
-            CustomMessengerState.ERROR, "Null Pointer Exception".hardCode());
+        showCustomMessenger(CustomMessengerState.ERROR,
+            errorMessage ?? "Null Pointer Exception".hardCode());
         break;
       case ForbiddenFailure:
-        showCustomMessenger(CustomMessengerState.ERROR, "Forbidden".hardCode());
+        showCustomMessenger(
+            CustomMessengerState.ERROR, errorMessage ?? "Forbidden".hardCode());
         break;
       case UserNotFoundFailure:
-        showCustomMessenger(
-            CustomMessengerState.ERROR, " User Not Found".hardCode());
+        showCustomMessenger(CustomMessengerState.ERROR,
+            errorMessage ?? " User Not Found".hardCode());
         break;
       case NotFoundFailure:
-        showCustomMessenger(
-            CustomMessengerState.ERROR, " Not Found".hardCode());
+        showCustomMessenger(CustomMessengerState.ERROR,
+            errorMessage ?? " Not Found".hardCode());
         break;
       case BadRequestFailure:
-        showCustomMessenger(
-            CustomMessengerState.ERROR, "bad_request_failure".hardCode());
+        showCustomMessenger(CustomMessengerState.ERROR,
+            errorMessage ?? "bad_request_failure".hardCode());
         break;
       case UnauthorizedFailure:
-        showCustomMessenger(
-            CustomMessengerState.ERROR, " Unauthorized".hardCode());
+        showCustomMessenger(CustomMessengerState.ERROR,
+            errorMessage ?? " Unauthorized".hardCode());
         break;
 
       case NotAuthenticateFailure:
-        showCustomMessenger(
-            CustomMessengerState.ERROR, " Not authentication".hardCode());
+        showCustomMessenger(CustomMessengerState.ERROR,
+            errorMessage ?? " Not authentication".hardCode());
         break;
 
       default:
         showCustomMessenger(CustomMessengerState.ERROR,
-            " Bilinmeyen Bir Hata Oluştu".hardCode());
+            errorMessage ?? " Bilinmeyen Bir Hata Oluştu".hardCode());
         break;
     }
   }

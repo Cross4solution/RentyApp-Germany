b1399faf7e6eb8999bf451df87602fe08c33871e
yasinkrcn <23yasin1999@gmail.com>
2022-12-16 18:24:00 +0300

    home repo added to get it impl

--- lib/core/init/injection_container.dart
+++ lib/core/init/injection_container.dart
@@ -3,11 +3,13 @@ import 'package:rent_app_germany/core/utils/remote_data_source/data/repository/n
 import 'package:rent_app_germany/core/utils/remote_data_source/dio_manager.dart';
 import 'package:rent_app_germany/core/utils/remote_data_source/domain/repository/i_network_manager.dart';
 import 'package:rent_app_germany/features/home/controller/home_controller.dart';
+import 'package:rent_app_germany/features/home/repo/impl/home_repo_impl.dart';
 
 import '../../features/add_product/controller/product_controller.dart';
 import '../../features/auth/controller/register_controller.dart';
 import '../../features/auth/repo/impl/auth_repo_impl.dart';
 import '../../features/bottom_navigation_bar/view_model/bottom_navigation_bar_controller.dart';
+import '../../features/home/repo/home_repository.dart';
 import '../../features/message/controller/message_controller.dart';
 import '../_core_exports.dart';
 
@@ -20,11 +22,16 @@ Future<void> init() async {
   sl.registerLazySingleton<AuthRepository>(
     () => AuthRepositoryImpl(),
   );
+
+  sl.registerLazySingleton<HomeRepository>(
+    () => HomeRepoImpl(),
+  );
   sl.registerLazySingleton(() => BottomNavigationBarProvider());
 
-  sl.registerLazySingleton(() => HomeController());
   sl.registerLazySingleton(
       () => RegisterController(authRepository: sl<AuthRepository>()));
+  sl.registerLazySingleton(
+      () => HomeController(homeRepository: sl<HomeRepository>()));
   sl.registerLazySingleton(() => LoginController());
   sl.registerLazySingleton(() => ProductController());
   sl.registerLazySingleton(() => MessageController());

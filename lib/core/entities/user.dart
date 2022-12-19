84f9f2b7dbf28d57b64220f6cf0d6004315820f8
yasinkrcn <23yasin1999@gmail.com>
2022-12-19 10:46:36 +0300

    user model updated

--- lib/core/entities/user.dart
+++ lib/core/entities/user.dart
@@ -1,39 +1,85 @@
 import '../_core_exports.dart';
 
 class UserModel {
-  final String name;
-  final String email;
-  final String phoneNumber;
-  final String username;
-  final String password;
-  final bool isSeller;
-
   UserModel({
-    required this.name,
-    required this.email,
-    required this.phoneNumber,
-    required this.username,
-    required this.password,
-    required this.isSeller,
+     this.errorCode,
+     this.accessToken,
+     this.tokenType,
+     this.user,
   });
 
-  factory UserModel.fromMap(Map<String, dynamic> map) => UserModel(
-        name: map['name'],
-        email: map['email'],
-        phoneNumber: map['phoneNumber'],
-        username: map['username'],
-        password: map['password'],
-        isSeller: map['isSeller'],
+  final int? errorCode;
+  final String? accessToken;
+  final String? tokenType;
+  final User? user;
+
+  factory UserModel.fromJson(String str) => UserModel.fromMap(json.decode(str));
+
+  String toJson() => json.encode(toMap());
+
+  factory UserModel.fromMap(Map<String, dynamic> json) => UserModel(
+        errorCode: json["error_code"],
+        accessToken: json["access_token"],
+        tokenType: json["token_type"],
+        user: User.fromMap(json["user"]),
       );
 
   Map<String, dynamic> toMap() => {
-        'name': name,
-        'email': email,
-        'phoneNumber': phoneNumber,
-        'username': username,
-        'password': password,
-        'isSeller': isSeller,
+        "error_code": errorCode,
+        "access_token": accessToken,
+        "token_type": tokenType,
+        "user": user?.toMap(),
       };
+}
+
+class User {
+  User({
+     this.uuid,
+     this.name,
+     required this.username,
+     this.email,
+     this.storeId,
+     this.phoneNumber,
+     this.isSeller,
+     this.status,
+     required this.password,
+  });
+
+  final String? uuid;
+  final String? name;
+  final String username;
+  final String? email;
+  final dynamic storeId;
+  final String? phoneNumber;
+  final bool? isSeller;
+  final bool? status;
+  final String password;
+
+  factory User.fromJson(String str) => User.fromMap(json.decode(str));
 
   String toJson() => json.encode(toMap());
+
+  factory User.fromMap(Map<String, dynamic> json) => User(
+        uuid: json["uuid"],
+        name: json["name"],
+        username: json["username"],
+        email: json["email"],
+        storeId: json["store_id"],
+        phoneNumber: json["phoneNumber"],
+        isSeller: json["isSeller"],
+        status: json["status"],
+        password: json["password"],
+      );
+
+  Map<String, dynamic> toMap() => {
+        "uuid": uuid,
+        "name": name,
+        "username": username,
+        "email": email,
+        "store_id": storeId,
+        "phoneNumber": phoneNumber,
+        "isSeller": isSeller,
+        "status": status,
+        "password": password,
+      };
 }
